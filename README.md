f3f1d660bc1c5644c0ea8da6d68c738ec582e2f3
lmaonator <lmaonator@gmail.com>
2021-12-10 00:37:26 +0100

    Create packaging files according to latest guide

---
+++ README.md
@@ -0,0 +1,4 @@
+# av1clip
+
+A cli script to create .webm clips using mpv, svt-av1 and ffmpeg
+
--- src/av1clip/av1clip.py
+++ src/av1clip/av1clip.py
@@ -1,4 +1,21 @@
 #!/usr/bin/env python3
+"""
+    av1clip - A cli script to create .webm clips using mpv, svt-av1 and ffmpeg
+    Copyright (C) 2021  lmaonator
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 import argparse
 import subprocess
 import json
